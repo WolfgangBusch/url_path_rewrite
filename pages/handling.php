3a7ff905a96ee8836e331f3dc03954eae828ed02
Wolfgang Busch <wolfgang@busch-dettum.de>
2019-06-23 11:56:51 +0200

    Add files via upload

--- pages/handling.php
+++ pages/handling.php
@@ -3,96 +3,97 @@
  * URL-Rewrite AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Dezember 2017
+ * @version Juni 2019
  */
 #
-$attr="Metadaten";
-$dname="&quot;Dateiname&quot;";
-$vname="&quot;Verzeichnisname&quot;";
-$urlbez="Custom URL";
+$attr='Metadaten';
+$dname='&quot;Dateiname&quot;';
+$vname='&quot;Verzeichnisname&quot;';
+$urlbez='Custom URL';
+$stg='style="color:green;"';
 #
 $string='
-<div><b>Eingabe/Aktualisierung der zusätzlichen Meta Infos:</b></div>
+<div><b>Eingabe/Aktualisierung der zusÃ¤tzlichen Meta Infos:</b></div>
 <ul>
     <li>'.$vname.' einer Kategorie [<code>'.REWRITER_DIR.'</code>]:
-        Eingabe im Menü <code style="color:green;">ändern</code>
+        Eingabe im MenÃ¼ <code '.$stg.'>Ã¤ndern</code>
         der Kategorie.</li>
     <li>'.$dname.' eines Artikels [<code>'.REWRITER_BASE.'</code>]:
-        Eingabe im Menü <code style="color:green;">'.$attr.'</code>
+        Eingabe im MenÃ¼ <code '.$stg.'>'.$attr.'</code>
         des Artikels.</li>
     <li>'.$urlbez.' eines Artikels [<code>'.REWRITER_URL.'</code>]:
-        Wird in jedem Kontextmenüs des Artikels automatsch neu erzeugt,
+        Wird in jedem KontextmenÃ¼s des Artikels automatsch neu erzeugt,
         ist also auch vor der Eingabe von Artikelinhalten schon
         vorhanden.</li>
 </ul>
 <div><b>Anlegen einer neuen Kategorie:</b></div>
 <ul>
-    <li>Das Menü <code style="color:green;">
-        <i class="rex-icon rex-icon-add"></i></code> öffnen (ist nur
+    <li>Das MenÃ¼ <code '.$stg.'>
+        <i class="rex-icon rex-icon-add"></i></code> Ã¶ffnen (ist nur
         vorhanden, wenn Meta Infos der Kategorie definiert sind).</li>
-    <li>In das Eingabefeld '.$vname.' der Kategorie
-        [<code>'.REWRITER_DIR.'</code>] eingegeben.<br/>
-        Default-Wert: der darüber eingetragene Kategoriename
+    <li>In das Eingabefeld eingeben: '.$vname.' der Kategorie
+        [<code>'.REWRITER_DIR.'</code>].<br/>
+        Default-Wert: der darÃ¼ber eingetragene Kategoriename
         [<code>catname</code>]
         (falls leer: Kategorie-Id [<code>id</code>]).</li>
-    <li>Mit dem Eintritt in das Kontextmenü eines Artikels dieser
-        Kategorie wird der '.$vname.' überprüft (erlaubte Zeichen,
+    <li>Mit dem Eintritt in das KontextmenÃ¼ eines Artikels dieser
+        Kategorie wird der '.$vname.' Ã¼berprÃ¼ft (erlaubte Zeichen,
         Eindeutigkeit) und ggf. korrigiert, bevor er als Pfadanteil
         in den URL aufgenommen wird. Dabei wird der '.$vname.' der
-        Sprache des gerade bearbeiteten Artikels für alle anderen
-        Sprachversionen übernommen.</li>
+        Sprache des gerade bearbeiteten Artikels auch fÃ¼r alle anderen
+        Sprachversionen Ã¼bernommen.</li>
 </ul>
 <div><b>Anlegen eines neuen Artikels:</b></div>
 <ul>
-    <li>Im Eingabefeld den '.$dname.' [<code>'.REWRITER_BASE.'</code>]
-        eingeben.<br/>
+    <li>In das Eingabefeld eintragen: '.$dname.' des Artikels
+        [<code>'.REWRITER_BASE.'</code>].<br/>
         Default-Wert: <code>name.html</code> (normaler Artikel,
         falls leer: <code>id.html</code>) bzw. <code>index.html</code>
         (Startartikel).<br/>
-        Namenserweiterung bzw. Startartikelname können auch anders
+        Namenserweiterung bzw. Startartikelname kÃ¶nnen auch anders
         konfiguriert werden.</li>
-    <li>Der eingegebene Wert wird überprüft (erlaubte Zeichen,
-        Eindeutigkeit), ggf. korrigiert und parallel für alle
+    <li>Der eingegebene Wert wird Ã¼berprÃ¼ft (erlaubte Zeichen,
+        Eindeutigkeit), ggf. korrigiert und parallel fÃ¼r alle
         Sprachversionen gespeichert.</li>
-    <li>Der zugehörige '.$urlbez.' [<code>'.REWRITER_URL.'</code>] wird
-        durch den Wechsel in ein anderes Kontextmenü des Artikels (z.B.
-        durch Rückkehr in den Editiermodus) automatisch erzeugt.</li>
+    <li>Der zugehÃ¶rige '.$urlbez.' [<code>'.REWRITER_URL.'</code>] wird
+        durch den Wechsel in ein anderes KontextmenÃ¼ des Artikels
+        automatisch erzeugt, z. B. durch RÃ¼ckkehr in den Editiermodus.</li>
 </ul>
 <div><b>Kopieren eines Artikels:</b></div>
 <ul>
-    <li>Für den kopierten Artikel werden automatisch ein '.$dname.'
-        [<code>'.REWRITER_BASE.'</code>] und ein zugehöriger '.$urlbez.'
+    <li>FÃ¼r den kopierten Artikel werden automatisch ein '.$dname.'
+        [<code>'.REWRITER_BASE.'</code>] und ein zugehÃ¶riger '.$urlbez.'
         [<code>'.REWRITER_URL.'</code>] erzeugt.</li>
 </ul>
 <div><b>Verschieben eines Artikels:</b></div>
 <ul>
-    <li>Für den verschobenen Artikel bleibt der '.$dname.'
+    <li>FÃ¼r den verschobenen Artikel bleibt der '.$dname.'
         [<code>'.REWRITER_BASE.'</code>] erhalten.</li>
     <li>Im Falle eines Startartikels bleibt ebenso der '.$vname.'
         [<code>'.REWRITER_DIR.'</code>] der Kategorie erhalten.</li>
     <li>Der '.$urlbez.' [<code>'.REWRITER_URL.'</code>] wird automatisch
-        gemäß dem neuen &quot;Pfad&quot; ersetzt.</li>
+        gemÃ¤ÃŸ dem neuen &quot;Pfad&quot; ersetzt.</li>
     <li>Nach dem Verschieben eines Startartikels wird der '.$urlbez.'
-        jedes Kindartikels erst durch Eintritt in dessen Kontextmenü
+        jedes Kindartikels erst durch Eintritt in dessen KontextmenÃ¼
         entsprechend angepasst.</li>
 </ul>
 <div><b>Umwandeln eines Artikels in einen Startartikel:</b></div>
 <ul>
-    <li>Mit dem Eintritt in das Kontextmenü des neuen Startartikels bzw.
+    <li>Mit dem Eintritt in das KontextmenÃ¼ des neuen Startartikels bzw.
         des neuen Kindartikels (und ehemaligen Startartikels) werden
         jeweils automatisch ein '.$dname.' [<code>'.REWRITER_BASE.'</code>]
-        und der zugehörige '.$urlbez.' [<code>'.REWRITER_URL.'</code>]
+        und der zugehÃ¶rige '.$urlbez.' [<code>'.REWRITER_URL.'</code>]
         angelegt.</li>
 </ul>
-<div><b>Utility-Funktionen für den Frontend:</b></div>
+<div><b>Utility-Funktionen fÃ¼r den Frontend:</b></div>
 <ul>
     <li><code>echo select_lang();</code> &nbsp; liefert im aktuellen
-        Artikel ein select-Menü zur Sprachauswahl.</li>
+        Artikel ein select-MenÃ¼ zur Sprachauswahl.</li>
     <li><code>echo select_url();</code> &nbsp; liefert im aktuellen
-        Artikel ein select-Menü zum Wechsel zwischen konfigurierter
-        URL-Form und Redaxo-Standard-URL, wobei die gewählte Sprache
-        beibehalten wird</li>
+        Artikel ein select-MenÃ¼ zum Wechsel zwischen konfigurierter
+        URL-Form und Redaxo-Standard-URL, wobei die gewÃ¤hlte Sprache
+        beibehalten wird.</li>
 </ul>
 ';
-echo utf8_encode($string);
+echo $string;
 ?>
---
+++ pages/rewrite.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * URL-Rewrite Addon
+ * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
+ * @package redaxo5
+ * @version Juni 2019
+ */
+#
+$stx='style="padding-left:20px;"';
+#
+$string='
+<div><b>Setzen eines Wunsch-URLs</b></div>
+<div '.$stx.'>Ein URL hat in Redaxo die Standardform
+<code>index.php?article_id=ID&amp;clang=CID</code> mit Verweis auf
+die Artikel-Id und die Sprach-Id. In der Regel soll stattdessen
+jeder Artikel einen &quot;Wunsch-URL&quot; erhalten, der Hinweise
+gibt auf Artikelinhalt, Themenkategorie, Site-Struktur o. Ä.
+Zur Realisierung wird eine Funktion definiert, die den gewünschten
+URL am <code>Extension Point URL_REWRITE</code> zurück gibt. Im
+Backend wird diese Funktion nur im Content-Kontext eines Artikels
+(edit, metainfo, functions) aufgerufen, im Frontend nur innerhalb
+der Funktion <code>rex_getUrl($article_id,$clang_id)</code>.
+Letztere liefert dem Redakteur (z. B. in Templates oder Modulen)
+den URL eines Artikels und die Anzeige im Browser-Adressfeld.</div>
+<br/>
+<div><b>Rewrite-Mechanismus</b></div>
+<div '.$stx.'>Ein Link auf einen Artikel wird durch eine
+Umleitungsregel
+<div '.$stx.'>
+<tt>RewriteRule ^(.*)$ index.php?%{QUERY_STRING} [L]</tt>
+&nbsp; &nbsp; &nbsp; (Datei <tt>.htaccess</tt>)</div>
+an das Redaxo CMS übergeben. Links auf Dateien oder Verzeichnisse
+erfolgen ohne Umleitung. Die Umleitung führt zunächst auf den
+Site-Startartikel. Damit stattdessen der gewünschte Artikel
+angezeigt wird, muss die aktuelle Artikel-Id
+<code>rex_article::getCurrentId()</code> mit der Id des Artikels
+überschrieben werden. Bei mehrsprachigen Installationen muss
+auch die aktuelle Sprach-Id <code>rex_clang::getCurrentId()</code>
+durch die Sprach-Id des Artikels ersetzt werden. Artikel-Id
+und Sprach-Id sind aus dem URL des anzuzeigenden Artikels, d. h.
+aus der Variablen <code>$_SERVER[\'REQUEST_URI\']</code>, zu
+ermitteln.</div>
+';
+echo $string;
+?>
